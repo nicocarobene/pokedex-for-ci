13cd25c8a65b6f0c6060e6c64da9b0dace6c7a9d
Nicolas <nicolascarobene1@gmail.com>
2023-09-06 14:28:03 +0200

    change test

--- cypress/integration/app.js
+++ cypress/integration/app.js
@@ -3,7 +3,8 @@
 describe('Pokedex', function() {
   it('frontpage can be opened', function () {
     cy.visit('http://localhost:5000')
-    cy.contains('pikachu')
-    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo')
+
+    cy.get('body').should('contain', 'pikachu')
+    cy.get('body').should('contain', 'Pokémon and Pokémon character names are trademarks of Nintendo')
   })
 })
