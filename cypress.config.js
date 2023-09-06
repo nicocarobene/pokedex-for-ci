cecb83c9d755c358f32f6e295dd8280aee03f5d7
Nicolas <nicolascarobene1@gmail.com>
2023-09-06 14:44:37 +0200

    change config.cypresr for CI

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5000',
-  },
-  'supportFile': false
+    'supportFile': false,
+    'specPattern': 'cypress/integration/e2e/*.js'
+  }
 })
